bcf7bd03fef7a9a998936479acfc02c2a7e39283
dellsystem <ilostwaldo@gmail.com>
2012-05-27 16:32:36 -0400

    Delete comments

--- mcmun/settings.py
+++ mcmun/settings.py
@@ -123,10 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.markup',
     'mcmun',
     'cms',
-    # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
 )
 
 # A sample logging configuration. The only tangible logging
