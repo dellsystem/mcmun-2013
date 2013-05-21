87cd9e45efc2c015dd640fd4a5ea81273acf0f04
LucilleH <lucille.hua@gmail.com>
2013-05-21 07:12:28 +0400

    registration opens

--- mcmun/views.py
+++ mcmun/views.py
@@ -20,12 +20,11 @@ def home(request):
 def registration(request):
 	if request.method == 'POST':
 		form = RegistrationForm(request.POST)
-		print formset.errors
+
 		if form.is_valid():
 			# Simple spam-prevention technique
 			if not request.POST.get('address', '').startswith('http://'):
 				registered_school = form.save()
-				registered_school.pays_convenience_fee = True
 				registered_school.save()
 
 				# Send emails to user, stysis, myself
@@ -51,40 +50,6 @@ def registration(request):
 	}
 
 	return render(request, "registration.html", data)
-
-def nikhil(request):
-	if request.method == 'POST':
-		form = RegistrationForm(request.POST)
-
-		if form.is_valid():
-			# Simple spam-prevention technique
-			if not request.POST.get('address', '').startswith('http://'):
-				registered_school = form.save()
-				registered_school.save()
-
-				# Send emails to user, stysis, myself
-				registered_school.send_success_email()
-
-			data = {
-				'page': {
-					'long_name': 'Succcessful registration'
-				}
-			}
-
-			return render(request, "registration_success.html", data)
-	else:
-		form = RegistrationForm()
-
-	data = {
-		'form': form,
-		'page': {
-			'long_name': 'Registration',
-		},
-		'min_num_delegates': MIN_NUM_DELEGATES,
-		'max_num_delegates': MAX_NUM_DELEGATES,
-	}
-
-	return render(request, "nikhil.html", data)
 
 
 
